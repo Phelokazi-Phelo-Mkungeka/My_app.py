d5ce57112bbbf259b80b338d631c9ed04930b7bb
Phelokazi-Phelo-Mkungeka <2016278786@ufs4life.ac.za>
2025-03-17 18:35:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt  # Corrected import
+import matplotlib.pyplot as plt
 from io import BytesIO
 
 st.title("Random Time Series Generator")
